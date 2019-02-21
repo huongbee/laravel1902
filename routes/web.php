edccca5ddcd7dcd77b7361b82e29c2832e8383d2
Huong <admin@Apples-MacBook-Pro.local>
2019-02-21 20:50:39 +0700

    route get

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,30 @@
 |
 */
 
-Route::get('/', function () {
+Route::get('home', function () {
     return view('welcome');
 });
+Route::get('about',function(){
+    echo 'About Page';
+});
+
+// http://localhost:8000/list-product?page=1
+//  uri
+//  url
+// http://localhost:8000/list-product/page-1
+
+Route::get('list-product/page-{trang?}',function($p=1){
+    echo $p;
+    // print_r($_GET);
+    // echo $_GET['page'];
+})->where('trang','[0-9]+');
+
+
+Route::get('user/{name}/{age?}',function($ten, $tuoi=null){
+    echo $ten;
+    echo "<br>";
+    echo $tuoi;
+})->where([
+    'name'=>'[a-zA-Z-]+',
+    'age'=>'[0-9]+'
+]);
